c7c7fa93ee4a6952e6ff81466b77d22a988fd337
Rejoan2020 <rej.ash11@gmail.com>
2026-08-09 13:44:19 +0600

    updated info

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,7 +6,8 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white mb-0 mt-2">
       <div className="container p-12 flex justify-between">
-        <span><Image src='/lg.jpg' height={100} width={150} /></span>
+        <span><Image src='/images/logog.png' height={60} width={150}
+          className='py-2 h-16 rounded-md border border-zinc-800' /></span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
